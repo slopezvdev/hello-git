1c4716a964a77fdde51f4e352ab4d39667178942
slopezvdev <slopezvdev@gmail.com>
2023-10-13 12:59:12 +0200

    Se actualizan los apuntes

--- hellogit.py
+++ hellogit.py
@@ -6,4 +6,8 @@ git status para ver el estado de nuestro proyecto
 git add para añadir
 git add . añade todos los ficheros pendientes
 git checkout hellogit.py devuelve el fichero al ultimo commit
+git log para ver todos los commits
+git log --graph para ver mas grafico
+git log --graph --pretty=oneline sale a una linea
+git log --graph --decorate --all --oneline el hash sale resumido y sale a una linea
 '''
