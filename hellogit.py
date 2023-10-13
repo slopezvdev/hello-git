6e5922e79573ec19a366aada792c928cc497ccef
slopezvdev <slopezvdev@gmail.com>
2023-10-13 12:47:11 +0200

    Este es mi segundo commit

--- hellogit.py
+++ hellogit.py
@@ -1,1 +1,9 @@
 print("Hello Git!")
+
+'''
+git init para inicializar git en el proyecto
+git status para ver el estado de nuestro proyecto
+git add para añadir
+git add . añade todos los ficheros pendientes
+
+'''
