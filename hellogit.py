3784f3065a01e11bcb2a0a68236b40959cb5bc01
slopezvdev <slopezvdev@gmail.com>
2023-10-13 12:54:29 +0200

    Se actualiza el texto del print

--- hellogit.py
+++ hellogit.py
@@ -1,9 +1,9 @@
-print("Hello Git!")
+print("New Hello Git!")
 
 '''
 git init para inicializar git en el proyecto
 git status para ver el estado de nuestro proyecto
 git add para añadir
 git add . añade todos los ficheros pendientes
-
+git checkout hellogit.py devuelve el fichero al ultimo commit
 '''
