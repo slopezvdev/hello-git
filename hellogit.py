0c869bc7d675927568e5ba55188d464ce2a885f1
slopezvdev <slopezvdev@gmail.com>
2023-10-13 13:17:21 +0200

    Se añade .gitignore correctamente :)

--- hellogit.py
+++ hellogit.py
@@ -1,6 +1,7 @@
 print("New Hello Git!")
 
 '''
+touch para crear ficero
 git init para inicializar git en el proyecto
 git status para ver el estado de nuestro proyecto
 git add para añadir
@@ -11,4 +12,9 @@ git log --graph para ver mas grafico
 git log --graph --pretty=oneline sale a una linea
 git log --graph --decorate --all --oneline el hash sale resumido y sale a una linea
 git config --global alias.tree "log --graph --decorate --all --oneline" para guardar esa forma de ver los commits con un alias
+git tree para usarlo
+touch .gitignore para crear un fichero donde vamos a meter otros ficheros que no queremos tener en cuenta en git
+en el .gitignore ponemos **/ y el nombre del archivo a ignorar, como **/.DS_Store (es un archivo que genera automaticamente mac en todas las carpetas)
+el .gitignore se debe añadir a git
+
 '''
